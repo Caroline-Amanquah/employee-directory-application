9e83be8844678f241ac9cf5f891e710ad4a72af2
Caroline Amanquah <carolineamanquah@Carolines-MacBook-Air.local>
2024-05-06 18:37:23 +0100

    CRUD Operations completed using postman and Mongodb Compass

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -2,7 +2,20 @@
 
 import Employee from "../model/employeeModel.js"
 
-// Function to create a new employee entry in the database
+export const getAllEmployees = async (req, res) => {
+    try {
+      const employeeData = await Employee.find();
+      if (!employeeData || employeeData.length === 0) {
+        return res.status(404).json({ message: "Employee data not found." });
+      }
+      res.status(200).json(employeeData);
+    } catch (error) {
+      res.status(500).json({ errorMessage: error.message });
+    }
+};
+
+
+// Create endpoint
 export const create = async (req, res) => {
     try {
         const newEmployee = new Employee(req.body); // Creating a new employee from request body
@@ -21,4 +34,68 @@ export const create = async (req, res) => {
         res.status(500).json({ errorMessage: error.message }); // Handling errors
     }
 };
+
+// Read endpoint 
+
+export const getEmployeeById = async (req, res) => {
+    try {
+        const id = req.params.id;  // Retrieving the user ID from request parameters
+        const employeeExist = await Employee.findById(id);  // Using Mongoose to find the user by ID
+
+        if (!employeeExist) {
+            // If no user is found, send a 404 response with an error message
+            return res.status(404).json({ message: "Employee not found." });
+        }
+
+        // If the user is found, send a 200 response with the user data
+        res.status(200).json(employeeExist);
+    } catch (error) {
+        // If an error occurs during the operation, send a 500 response with the error message
+        res.status(500).json({ errorMessage: error.message });
+    }
+};
+
+// Update endpoint
+
+export const update = async (req, res) => {
+    try {
+        const id = req.params.id; // Extracting the user ID from the request parameters
+        const employeeExist = await Employee.findById(id); // Checking if the user exists in the database
+
+        if (!employeeExist) {
+            // If the user does not exist, return a 404 error with a message
+            return res.status(404).json({ message: "Employee not found." });
+        }
+
+        // If the user exists, update the user data with the new data from req.body
+        const updatedData = await Employee.findByIdAndUpdate(id, req.body, { new: true });
+        // Respond with the updated data
+        res.status(200).json(updatedData);
+    } catch (error) {
+        // If an error occurs, return a 500 error with the error message
+        res.status(500).json({ errorMessage: error.message });
+    }
+};
+
+//Delete Endpoint
+
+export const deleteEmployee = async (req, res) => {
+    try {
+        const id = req.params.id;  // Retrieve the ID from the request parameters
+        const employeeExist = await Employee.findById(id);  // Check if the user exists in the database
+
+        if (!employeeExist) {
+            // If the user does not exist, return a 404 Not Found response
+            return res.status(404).json({ message: "Employee not found." });
+        }
+
+        // If the user exists, delete the user
+        await Employee.findByIdAndDelete(id);
+        // Return a success message
+        res.status(200).json({ message: "Employee deleted successfully." });
+    } catch (error) {
+        // If an error occurs, return a 500 Internal Server Error response
+        res.status(500).json({ errorMessage : error.message });
+    }
+    };
 
--- server/routes/employeeRoutes.js
+++ server/routes/employeeRoutes.js
@@ -1,12 +1,19 @@
 // defines the endpoints of the application and also maps them to specific controller methods 
 
-//create endpoint:
+// Create endpoint:
 
 import express from "express";
-import { create } from "../controllers/employeeController.js";
+import { getAllEmployees, create, deleteEmployee, getEmployeeById, update } from "../controllers/employeeController.js";
 
 const route = express.Router();
 
-route.post("/employees", create);
+route.post("/employee", create); //e.g  http://localhost:5000/api/employee/
+route.get("/employees", getAllEmployees);
+route.get("/employee/:id", getEmployeeById); //e.g  http://localhost:5000/api/employee/66390a86b15cd0ffb3bc4a7d
+route.put("/update/employee/:id", update); // e.g http://localhost:5000/api/update/employee/66390a86b15cd0ffb3bc4a7d
+route.delete("/delete/employee/:id", deleteEmployee); // e.g http://localhost:5000/api/delete/employee/66390a86b15cd0ffb3bc4a7d
+
+
 
 export default route;
+
