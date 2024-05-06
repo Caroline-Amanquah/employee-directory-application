5b9692488be2d9fd2418a63288915e28b96ee387
Caroline Amanquah <carolineamanquah@Carolines-MacBook-Air.local>
2024-05-06 19:16:58 +0100

    client created

---
+++ client/src/getEmployee/Employee.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Employee = () => {
+  return (
+    <div>Employee Table</div>
+  )
+}
+
+export default Employee
+
+/*
+rafce creates this function automatically:
+
+import React from 'react'
+
+const Employee = () => {
+  return (
+    <div>Employee</div>
+  )
+}
+
+export default Employee
+
+*/
+
